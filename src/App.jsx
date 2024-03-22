e36c997cca6f8cf5a1051b1b8238c070972d6b8b
Zioun <jionkhan0@gmail.com>
2024-03-23 03:28:17 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
 import './App.css'
 import HookForm from './Components/HookForm/HookForm'
 import RefForm from './Components/RefForm/RefForm'
+import ReusableForm from './Components/ReusableForm/ReusableForm'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
 import StateFullForm from './Components/StateFullForm/StateFullForm'
 
 function App() {
 
+  const handleLogin = (data) => {
+    console.log("Login",data)
+  }
+
+  const handleUpdate = (data) => {
+    console.log("Update",data)
+  }
+  
+
   return (
     <>
-      <h2>Form</h2>
+      <h1>Form</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFullForm></StateFullForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm handleSubmit={handleLogin} formTitle={"Login"} formBtn={"Login"}>
+        <div>
+          <h2>Login</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, laboriosam.</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm handleSubmit={handleUpdate} formTitle={"Update"} formBtn={"Update"}>
+        <div>
+          <h2>Update</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci, aliquid.</p>
+        </div>
+      </ReusableForm>
     </>
   )
 }
